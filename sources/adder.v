1c76921a73992af0cf74d01295cab3f5f4f28e20
Hassan-Raza-164 <106656475+Hassan-Raza-164@users.noreply.github.com>
2022-11-19 17:42:21 +0500

    Update adder.v
    add the wire for carry bit

--- sources/adder.v
+++ sources/adder.v
@@ -1,8 +1,8 @@
-module adder(a,b,s);
+module adder(a,b,s,c);
   
     input  wire [1:0] a,b;
     output wire [1:0] s;
-    
-    assign s = a+b;
+    output            c;
+    assign {c,s} = a+b;
     
 endmodule
